fcdaa28cd4ab92c0e66d45ca6a978f1dd5d9d553
rd_t.syoji <t.syoji@intercom-rdc.co.jp>
2021-10-25 13:28:36 +0900

    Initial commit

---
+++ app/src/main/kotlin/jp/techacademy/takafumi/shouji/taskapp/InputActivity.kt
@@ -0,0 +1,171 @@
+package jp.techacademy.takafumi.shouji.taskapp
+
+import android.app.AlarmManager
+import android.app.DatePickerDialog
+import android.app.PendingIntent
+import android.app.TimePickerDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import android.view.View
+import io.realm.Realm
+import jp.techacademy.takafumi.shouji.taskapp.databinding.ActivityInputBinding
+import java.util.*
+
+/**
+ * タスクの入力画面
+ */
+class InputActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityInputBinding
+
+    private var mYear = 0
+    private var mMonth = 0
+    private var mDay = 0
+    private var mHour = 0
+    private var mMinute = 0
+    private var mTask: Task? = null
+
+    /**
+     * 日付設定が押されたときの処理
+     */
+    private val mOnDateClickListener = View.OnClickListener {
+        val datePickerDialog = DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                mYear = year
+                mMonth = month
+                mDay = dayOfMonth
+                val dateString = mYear.toString() + "/" + String.format("%02d", mMonth + 1) + "/" + String.format("%02d", mDay)
+                binding.input.dateButton.text = dateString
+            }, mYear, mMonth, mDay)
+        datePickerDialog.show()
+    }
+
+    /**
+     * 時刻設定が押されたときの処理
+     */
+    private val mOnTimeClickListener = View.OnClickListener {
+        val timePickerDialog = TimePickerDialog(this,
+            TimePickerDialog.OnTimeSetListener { _, hour, minute ->
+                mHour = hour
+                mMinute = minute
+                val timeString = String.format("%02d", mHour) + ":" + String.format("%02d", mMinute)
+                binding.input.timesButton.text = timeString
+            }, mHour, mMinute, false)
+        timePickerDialog.show()
+    }
+
+    /**
+     * 決定ボタンが押されたときの処理
+     */
+    private val mOnDoneClickListener = View.OnClickListener {
+        addTask()
+        finish()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityInputBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // ActionBarを設定する
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        if (supportActionBar != null) {
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        }
+
+        // UI部品の設定
+        binding.input.dateButton.setOnClickListener(mOnDateClickListener)
+        binding.input.timesButton.setOnClickListener(mOnTimeClickListener)
+        binding.input.doneButton.setOnClickListener(mOnDoneClickListener)
+
+        // EXTRA_TASKからTaskのidを取得して、 idからTaskのインスタンスを取得する
+        val intent = intent
+        val taskId = intent.getIntExtra(EXTRA_TASK, -1)
+        val realm = Realm.getDefaultInstance()
+        mTask = realm.where(Task::class.java).equalTo("id", taskId).findFirst()
+        realm.close()
+
+        if (mTask == null) {
+            // 新規作成の場合
+            val calendar = Calendar.getInstance()
+            mYear = calendar.get(Calendar.YEAR)
+            mMonth = calendar.get(Calendar.MONTH)
+            mDay = calendar.get(Calendar.DAY_OF_MONTH)
+            mHour = calendar.get(Calendar.HOUR_OF_DAY)
+            mMinute = calendar.get(Calendar.MINUTE)
+        } else {
+            // 更新の場合
+            binding.input.titleEditText.setText(mTask!!.title)
+            binding.input.contentEditText.setText(mTask!!.contents)
+
+            val calendar = Calendar.getInstance()
+            calendar.time = mTask!!.date
+            mYear = calendar.get(Calendar.YEAR)
+            mMonth = calendar.get(Calendar.MONTH)
+            mDay = calendar.get(Calendar.DAY_OF_MONTH)
+            mHour = calendar.get(Calendar.HOUR_OF_DAY)
+            mMinute = calendar.get(Calendar.MINUTE)
+
+            val dateString = mYear.toString() + "/" + String.format("%02d", mMonth + 1) + "/" + String.format("%02d", mDay)
+            val timeString = String.format("%02d", mHour) + ":" + String.format("%02d", mMinute)
+
+            binding.input.dateButton.text = dateString
+            binding.input.timesButton.text = timeString
+        }
+    }
+
+    /**
+     * タスクを追加する処理
+     */
+    private fun addTask() {
+        // Realmに保存
+        val realm = Realm.getDefaultInstance()
+
+        realm.beginTransaction()
+
+        if (mTask == null) {
+            // 新規作成の場合
+            mTask = Task()
+
+            val taskRealmResults = realm.where(Task::class.java).findAll()
+
+            val identifier: Int =
+                if (taskRealmResults.max("id") != null) {
+                    taskRealmResults.max("id")!!.toInt() + 1
+                } else {
+                    0
+                }
+            mTask!!.id = identifier
+        }
+
+        val title = binding.input.titleEditText.text.toString()
+        val content = binding.input.contentEditText.text.toString()
+
+        mTask!!.title = title
+        mTask!!.contents = content
+        val calendar = GregorianCalendar(mYear, mMonth, mDay, mHour, mMinute)
+        val date = calendar.time
+        mTask!!.date = date
+
+        realm.copyToRealmOrUpdate(mTask!!)
+        realm.commitTransaction()
+
+        realm.close()
+
+        // アラームをセット
+        val resultIntent = Intent(applicationContext, TaskAlarmReceiver::class.java)
+        resultIntent.putExtra(EXTRA_TASK, mTask!!.id)
+        val resultPendingIntent = PendingIntent.getBroadcast(
+            this,
+            mTask!!.id,
+            resultIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, resultPendingIntent)
+    }
+}
---
+++ app/src/main/kotlin/jp/techacademy/takafumi/shouji/taskapp/MainActivity.kt
@@ -0,0 +1,143 @@
+package jp.techacademy.takafumi.shouji.taskapp
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
+import io.realm.Realm
+import io.realm.RealmChangeListener
+import io.realm.Sort
+import jp.techacademy.takafumi.shouji.taskapp.databinding.ActivityMainBinding
+import java.util.*
+
+const val EXTRA_TASK = "jp.techacademy.takafumi.shouji.taskapp.TASK"
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mTaskAdapter: TaskAdapter
+    private lateinit var mRealm: Realm
+    private val mRealmListener = RealmChangeListener<Realm> { reloadListView() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fab.setOnClickListener { view ->
+            val intent = Intent(this, InputActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Realmの設定
+        mRealm = Realm.getDefaultInstance()
+        mRealm.addChangeListener(mRealmListener)
+
+        // ListViewの設定
+        mTaskAdapter = TaskAdapter(this)
+
+        // ListViewをタップしたときの処理
+        binding.listView1.setOnItemClickListener { parent, view, position, id ->
+            // 入力・編集する画面に遷移させる
+            val task = parent.adapter.getItem(position) as Task
+            val intent = Intent(this, InputActivity::class.java)
+            intent.putExtra(EXTRA_TASK, task.id)
+            startActivity(intent)
+        }
+
+        // ListViewを長押ししたときの処理
+        binding.listView1.setOnItemLongClickListener { parent, view, position, id ->
+            // タスクを削除する
+            val task = parent.adapter.getItem(position) as Task
+
+            // ダイアログを表示する
+            val builder = AlertDialog.Builder(this)
+
+            builder.setTitle("削除")
+            builder.setMessage(task.title + "を削除しますか")
+
+            builder.setPositiveButton("OK"){_, _ ->
+                // Realmからデータを削除
+                val results = mRealm.where(Task::class.java).equalTo("id", task.id).findAll()
+                mRealm.beginTransaction()
+                results.deleteAllFromRealm()
+                mRealm.commitTransaction()
+
+                // アラームを解除
+                val resultIntent = Intent(applicationContext, TaskAlarmReceiver::class.java)
+                val resultPendingIntent = PendingIntent.getBroadcast(
+                    this,
+                    task.id,
+                    resultIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+                )
+
+                val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+                alarmManager.cancel(resultPendingIntent)
+
+                reloadListView()
+            }
+
+            builder.setNegativeButton("CANCEL", null)
+
+            val dialog = builder.create()
+            dialog.show()
+
+            true
+        }
+
+        reloadListView()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun reloadListView() {
+        // Realmデータベースから、「すべてのデータを取得して新しい日時順に並べた結果」を取得
+        val taskRealmResults = mRealm.where(Task::class.java).findAll().sort("date", Sort.DESCENDING)
+
+        // 上記の結果を、TaskListとしてセットする
+        mTaskAdapter.mTaskList = mRealm.copyFromRealm(taskRealmResults)
+
+        // TaskのListView用のアダプタに渡す
+        binding.listView1.adapter = mTaskAdapter
+
+        // 表示を更新するために、アダプターにデータが変更されたことを知らせる
+        mTaskAdapter.notifyDataSetChanged()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mRealm.close()
+    }
+
+    private fun addTaskForTest() {
+        val task = Task()
+        task.title = "作業"
+        task.contents = "プログラムを書いてPUSHする"
+        task.date = Date()
+        task.id = 0
+        mRealm.beginTransaction()
+        mRealm.copyToRealmOrUpdate(task)
+        mRealm.commitTransaction()
+    }
+}
